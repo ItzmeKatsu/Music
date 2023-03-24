09b79d7562232f2f4838a531ddaeeb556bc00a96
playerxdx <128796714+playerxdx@users.noreply.github.com>
2023-03-24 16:02:49 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001670720917"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Krypton)
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Krypton")
 
 # Your User ID.
 OWNER_ID = list(
